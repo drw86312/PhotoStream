11b468bd2607f8294dd5b1a417425f2273a61b9d
drw86312 <david.warner4@gmail.com>
2015-05-31 16:08:11 -0500

    Initial Commit

---
+++ PhotoStream/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoStream
+//
+//  Created by David Warner on 5/31/15.
+//  Copyright (c) 2015 dwarner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
